f724c969cb47997e7a9949cb829a0d89cedf55b4
Rohit M Patil <roniepatil@gmail.com>
2023-10-26 23:38:34 -0400

    Gaussian_Noise

--- Edit_Image/Gaussian_Noise.py
+++ Edit_Image/Gaussian_Noise.py
@@ -20,12 +20,17 @@ image = cv2.imread('Edit_Image/messi.png')
 noisy_image = addGaussainNoise(image)
 
 # Apply Gaussian filter
+# Gaussian Filter: This is a low-pass filter that attenuates high-frequency noise. 
+# It's effective for Gaussian noise but can blur sharp edges in the image.
 gaussian_filtered = cv2.GaussianBlur(noisy_image, (5, 5), 0)
 
 # Apply Bilateral filter
+# Bilateral Filter: This filter considers both the spatial and intensity differences between pixels. 
+# It can effectively reduce noise while preserving edges.
 bilateral_filtered = cv2.bilateralFilter(noisy_image, d=9, sigmaColor=75, sigmaSpace=75)
 
 # Non-local Means Denoising
+# Non-Local Means Denoising: This algorithm replaces the value of a pixel with a weighted average of similar pixels from the entire image.
 non_local_MD = cv2.fastNlMeansDenoisingColored(noisy_image, None, 10, 10, 7, 21)
 
 
